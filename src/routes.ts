7fc70350c42c71fc57a2ffba83d3393ebd2526dd
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-01-28 22:11:41 -0300

    fazendo a chamada das rotas das notificações no server

--- src/routes.ts
+++ src/routes.ts
@@ -1,43 +1,42 @@
-import dayjs from 'dayjs';
-import { FastifyInstance } from 'fastify';
-import { z } from 'zod';
+import dayjs from "dayjs";
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "./lib/prisma";
 
-
 export async function appRoutes(app: FastifyInstance) {
-  app.post('/habits', async (request) => {
+  app.post("/habits", async (request) => {
     const createHabitBody = z.object({
       title: z.string(),
-      weekDays: z.array(z.number()).min(0).max(6),
-    })
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
 
     const { title, weekDays } = createHabitBody.parse(request.body);
 
-    const today = dayjs().startOf('day').toDate();
+    const today = dayjs().startOf("day").toDate();
 
     await prisma.habit.create({
       data: {
         title,
         created_at: today,
         weekDays: {
-          create: weekDays.map(weekDay => {
+          create: weekDays.map((weekDay) => {
             return {
               week_day: weekDay,
-            }
-          })
-        }
-      }
+            };
+          }),
+        },
+      },
     });
   });
 
-  app.get('/day', async (request) => {
+  app.get("/day", async (request) => {
     const getDayParams = z.object({
       date: z.coerce.date(),
     });
 
     const { date } = getDayParams.parse(request.query);
-    const parsedDate = dayjs(date).startOf('day');
-    const weekDay = parsedDate.get('day');
+    const parsedDate = dayjs(date).startOf("day");
+    const weekDay = parsedDate.get("day");
 
     const possibleHabits = await prisma.habit.findMany({
       where: {
@@ -47,50 +46,51 @@ export async function appRoutes(app: FastifyInstance) {
         weekDays: {
           some: {
             week_day: weekDay,
-          }
-        }
-      }
+          },
+        },
+      },
     });
 
-    const day = await prisma.day.findUnique({
+    const day = await prisma.day.findFirst({
       where: {
         date: parsedDate.toDate(),
       },
       include: {
         dayHabits: true,
-      }
-    })
+      },
+    });
 
-    const completedHabits = day?.dayHabits.map(dayHabit => {
-      return dayHabit.habit_id;
-    }) ?? [];
+    const completedHabits =
+      day?.dayHabits.map((dayHabit) => {
+        return dayHabit.habit_id;
+      }) ?? [];
 
     return {
       possibleHabits,
       completedHabits,
-    }
+    };
   });
 
-  app.patch('/habits/:id/toggle', async (request) => {
+  app.patch("/habits/:id/toggle", async (request) => {
     const toggleHabitParams = z.object({
       id: z.string().uuid(),
     });
 
     const { id } = toggleHabitParams.parse(request.params);
 
-    const today = dayjs().startOf('day').toDate();
+    const today = dayjs().startOf("day").toDate();
 
     let day = await prisma.day.findUnique({
       where: {
         date: today,
-      }
+      },
     });
 
-    if(!day) {
+    if (!day) {
       day = await prisma.day.create({
         data: {
           date: today,
-        }
+        },
       });
     }
 
@@ -99,27 +99,27 @@ export async function appRoutes(app: FastifyInstance) {
         day_id_habit_id: {
           day_id: day.id,
           habit_id: id,
-        }
-      }
+        },
+      },
     });
 
-    if(dayHabit) {
+    if (dayHabit) {
       await prisma.dayHabit.delete({
         where: {
           id: dayHabit.id,
-        }
+        },
       });
     } else {
       await prisma.dayHabit.create({
         data: {
           day_id: day.id,
           habit_id: id,
-        }
+        },
       });
     }
   });
 
-  app.get('/summary', async () => {
+  app.get("/summary", async () => {
     const summary = await prisma.$queryRaw`
       SELECT 
         D.id, 
@@ -146,4 +146,3 @@ export async function appRoutes(app: FastifyInstance) {
     return summary;
   });
 }
-
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
-import Fastify from 'fastify';
-import cors from '@fastify/cors';
-import { prisma } from './lib/prisma';
-import { appRoutes } from './routes';
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
+import { notificationRoutes } from "./notification-routes";
 
 const app = Fastify();
 
 app.register(cors);
 app.register(appRoutes);
+app.register(notificationRoutes);
 
-app.listen({
-  port: 3333,
-  host: '0.0.0.0',
-}).then(() => {
-  console.log('HTTP Server Running');
-});
+app
+  .listen({
+    port: 3333,
+    host: "0.0.0.0",
+  })
+  .then(() => {
+    console.log("HTTP Server Running");
+  });
